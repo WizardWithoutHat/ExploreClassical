df734a325d09f9ba4b36787b1d2f6c5a83be3b96
fzk830 <kristian_h.olsen@hotmail.com>
2015-04-30 15:00:24 +0200

    Index.php
    Ny index php fil

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "gator4206";
+$username = "exc_admin";
+$password = "eSGq#muX9ddJ";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
+
+<html>
+	<head>
+	
+	</head>
+	
+	<body>
+	
+	</body>
+</html>
