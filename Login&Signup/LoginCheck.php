04bfb23743fa2d684bbc66e7f0a315b9aac6b1ca
fzk830 <kristian_h.olsen@hotmail.com>
2015-05-04 14:40:11 +0200

    Login + Signup
    Meget basic login og signup

---
+++ Login&Signup/LoginCheck.php
@@ -0,0 +1,38 @@
+﻿<html>
+	<head>
+		<title> Explore Classical </title>
+	</head>
+<body>
+<?php
+$conn = mysql_connect("localhost");
+mysql_select_db($conn);
+
+if($conn)
+{
+    print mysql_error();
+	
+	$datstring = mysql_num_rows(mysql_query
+	("
+		SELECT * FROM Users
+		WHERE `Username` = '$_POST[user_name]' AND `Password` = '$_POST[user_name]'
+	"));
+
+	if ($datstring == 0) // hvis den ikke finder nogle rækker i tabellen får brugeren en fejlmeddelelse.
+	{
+		echo "Username and Password do not match";
+		?>
+		<form method="post" action="Index.php">
+		<input type="submit" value="Back to Home Page"> </a>
+		</form>
+		<?php
+	}
+	else
+	{
+		?>
+		<!-- Hvad der end sker efter man har logget ind (redirect til profil?)-->		
+		<?php
+	}
+} 
+?>
+	</body>
+</html>
---
+++ Login&Signup/SignUpCheck.php
@@ -0,0 +1,40 @@
+﻿<?php
+$conn = mysql_connect("localhost");
+mysql_select_db($conn);
+if(!$conn)
+    print mysql_error();
+
+$Brugernavn = $_REQUEST["user_name"];
+$Password = $_REQUEST["pass_word"];
+// Gemmer indtastet data fra login i nye variabler.
+
+$datstring="select * from Users where Username='" . $Brugernavn . "'";
+	print mysql_error();
+$recordset = mysql_query($datstring);
+
+if (mysql_num_rows($recordset) == '0') // If lykke som tjekker om der står noget i tabellen.
+{
+	if ($Password !== "")	// Tjekker om der står noget i password feltet.
+	{
+		$insertSQL = "insert into Users (Username, Password) values ('$Brugernavn', '$Password')";  // indsætter de indtastet data i tabellen.
+		mysql_query ($insertSQL);
+		echo "Your profile has been created, welcome to explore classical. Press the Button to return to the front page.";
+	}
+	else
+	{
+		echo "Cant have empty password <br>";
+	}
+
+}
+else
+{
+	echo "Username is taken, please choose another. <br> <br>";
+}
+?>
+
+<html>
+<form method="post" action="Index.php">
+<input type="submit" value="Back to Home Page"> <br>
+<!-- Man bliver sendt tilbage til startsiden lige meget hvad tilsidst, men burde nemt kunne ændres -->
+</form>
+</html>
