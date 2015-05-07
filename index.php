ea2d423900f9a313c02b14a7f88765dc88d2fc77
Marioxorz <mariusbsamson@gmail.com>
2015-05-07 13:55:24 +0200

    retskrivning mm.

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
-$servername = "localhost";
-$username = "exc";
-$password = "gxez2G:Pwfd5";
+	$servername = "localhost";
+	$username = "exc";
+	$password = "gxez2G:Pwfd5";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password);
+	// Create connection
+	$conn = mysqli_connect($servername, $username, $password);
 
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+	// Check connection
+	if (!$conn) 
+	{
+		die("Connection failed: " . mysqli_connect_error());
+	}
 ?>
 
 <html>
@@ -67,10 +68,10 @@ if (!$conn) {
 				<div class="col-sm-4"> 
 					<div class="container-fluid">	
 						<div class="page-header text-center">
-							<h1 class="text-important">Go Exploreing!</h1>
+							<h1 class="text-important">Go Exploring!</h1>
 						</div>
 						
-						<p> This website offers you access into the world of one of the greatest genres of music in the history of man, Classical Music. 
+						<p> This website offers you access to the world of one of the greatest genres of music in the history of man, Classical Music. 
 						Classical music has been around for centuries, and has usually been seen as the educated mans music, which has led to it being less prevalent in the modern scene.
 						This website aims to correct this by bringing you the wonders of the classical genre, to prove that this has more to offer to the modern society than you'd think. </p>
 					
@@ -86,7 +87,7 @@ if (!$conn) {
 					<div class="page-header text-center">
 						<h1 class="text-important">Here is a small taste of what you may discover</h1>
 					</div>
-					<h4 class="text-danger text-center">This music-track right here is currently chosen from only 5 different tracks, but is chosen at random, as to really make people discover something new!</h4>
+					<h4 class="text-danger text-center">This music track right here is currently chosen from only 5 different tracks, but is chosen at random, as to really make people discover something new!</h4>
 					<center>
 						<div class="container-fluid">
 							<iframe id="randomMusic" src="about:blank" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>
